bd977d7990a2f5a211e6dff1b6b16f04c5b09283
Andreina De Sola <andreinadesola@gmail.com>
2022-10-26 21:45:27 -0300

    reducir checkout

--- checkoutActions.js
+++ checkoutActions.js
@@ -6,11 +6,7 @@ const checkoutFormulario = document.getElementById('checkout-form');
 const checkoutNombre = document.getElementById('firstName');
 const checkoutApellido = document.getElementById('lastName');
 const checkoutEmail = document.getElementById('email');
-const checkoutDir = document.getElementById('address');
-const checkoutPais = document.getElementById('country');
-const checkoutProvincia = document.getElementById('state');
-const checkoutCP = document.getElementById('zip');
-const $buttonMailto = document.querySelector('#trucazo')
+
 
 
 //Funciones y métodos//
@@ -38,37 +34,86 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     if (checkoutFormulario) {
         checkoutFormulario.addEventListener('submit', (e) => {
-            
+
             const carrito = obtenerCarritoStorage();
-            
-            
+
+
             e.preventDefault();
+            if (validarForm() == false) {
+                return;
+            }
+            else {
+                // code fragment
+                const body = {
+                    service_id: 'service_b8lfzma',
+                    template_id: 'template_1v2snr7',
+                    user_id: '3gclg2lklH9bUm4t9',
+                    template_params: {
+                        'from_name': checkoutEmail.value,
+                        'to_name': checkoutNombre.value,
+                        'to_lastname': checkoutApellido.value
+                    }
+                };
+                sendEmail(body);
+            }
 
-            // code fragment
-            const body = {
-                service_id: 'service_b8lfzma',
-                template_id: 'template_1v2snr7',
-                user_id: '3gclg2lklH9bUm4t9',
-                template_params: {
-                    'from_name': checkoutEmail.value,
-                    'to_name':  checkoutNombre.value,
-                    'to_lastname': checkoutApellido.value,
-                    'dir': checkoutDir.value,
-                    'country': checkoutPais.value,
-                    'state': checkoutProvincia.value,
-                    'zip': checkoutCP.value,
-                    
-                }
-            };
 
-            sendEmail(body);
         })
     }
 
 
 })
 
-
+const validarForm = () => {
+    if (checkoutNombre.value.length == 0) {
+        Toastify({
+            text: "No has escrito un nombre.",
+            duration: 3000,
+            position: "center",
+            style: {
+                background: "linear-gradient(308deg, rgba(102, 16, 242, 1) 0%, rgba(144, 19, 254, 1) 50%)",
+            },
+        }).showToast();
+        return false;
+    }
+    else if (checkoutEmail.value.length == 0) {
+        Toastify({
+            text: "No has escrito un email.",
+            duration: 3000,
+            position: "center",
+            style: {
+                background: "linear-gradient(308deg, rgba(102, 16, 242, 1) 0%, rgba(144, 19, 254, 1) 50%)",
+            },
+        }).showToast();
+        return false;
+    }
+    else if (checkoutApellido.value.length == 0) {
+        Toastify({
+            text: "No has escrito un apellido.",
+            duration: 3000,
+            position: "center",
+            style: {
+                background: "linear-gradient(308deg, rgba(102, 16, 242, 1) 0%, rgba(144, 19, 254, 1) 50%)",
+            },
+        }).showToast();
+        return false;
+    }
+    else if (checkoutApellido.value.length == 0) {
+        Toastify({
+            text: "No has escrito un apellido.",
+            duration: 3000,
+            position: "center",
+            style: {
+                background: "linear-gradient(308deg, rgba(102, 16, 242, 1) 0%, rgba(144, 19, 254, 1) 50%)",
+            },
+        }).showToast();
+        return false;
+    }
+    else 
+    {
+        return true;
+    }
+}
 const pintarCarritoCheckout = (carrito) => {
     const contenedor = document.getElementById('checkout-carrito');
     if (contenedor) {
